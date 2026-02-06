a6e9d6ca70f9028c0faf2d611f135958a628231e
Louis Moretti <louis.moretti@epita.fr>
2026-02-06 11:43:32 +0100

    lets go

--- src/main/java/com/epita/creeps/Program.java
+++ src/main/java/com/epita/creeps/Program.java
@@ -6,8 +6,17 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String serverUri = "http://" + args[0] + ":" + args[1];
-        String init = serverUri + "/init/" + args[2];
-        Unirest.post(init).body("{}").asJson();
+        String serverUri = "http://" + args[0] + ":" + args[1] + "/";
+        String init = serverUri + "init/" + args[2];
+        var setup = Unirest.post(init).body("{}").asJson();
+        System.out.println(setup.getBody().toPrettyString());
+
+        String citizen1Id = setup.getBody().getObject().get("citizen1Id").toString();
+        String baseNoopCmd = serverUri + "command/" + args[2] + "/" + citizen1Id + "/noop";
+        var response = Unirest.post(baseNoopCmd).body("{}").asJson();
+        System.out.println(response.getBody().toPrettyString());
+
+        response = Unirest.get(serverUri + "statistics").asJson();
+        System.out.println(response.getBody().toPrettyString());
     }
 }
