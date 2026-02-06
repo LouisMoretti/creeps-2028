2648c02557039dc6aef044b114e6e5c3c1a0c6ca
Louis Moretti <louis.moretti@epita.fr>
2026-02-06 12:41:59 +0100

    base move

--- src/main/java/com/epita/creeps/Program.java
+++ src/main/java/com/epita/creeps/Program.java
@@ -1,19 +1,37 @@
 package com.epita.creeps;
 
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
 import kong.unirest.core.Unirest;
 
 public class Program {
+    static String url;
+    static String port;
+    static String login;
+    static String serverUri;
+
+    private static String getCitizenId(HttpResponse<JsonNode> setup, int n) {
+        return setup.getBody().getObject().get("citizen" + n + "Id").toString();
+    }
+
+    private static String getCmd(String citizenId, String cmd) {
+        return serverUri + "command/" + login + "/" + citizenId + "/" + cmd;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String serverUri = "http://" + args[0] + ":" + args[1] + "/";
-        String init = serverUri + "init/" + args[2];
+        url = args[0]; port = args[1]; login = args[2];
+        serverUri = "http://" + url + ":" + port + "/";
+
+
+//        System.out.println("Hello World!");
+        String init = serverUri + "init/" + login;
         var setup = Unirest.post(init).body("{}").asJson();
         System.out.println(setup.getBody().toPrettyString());
 
-        String citizen1Id = setup.getBody().getObject().get("citizen1Id").toString();
-        String baseNoopCmd = serverUri + "command/" + args[2] + "/" + citizen1Id + "/noop";
-        var response = Unirest.post(baseNoopCmd).body("{}").asJson();
+//        String citizen1Id = getCitizenId(setup, 1);
+//        String baseNoopCmd = serverUri + "command/" + login + "/" + citizen1Id + "/noop";
+        String baseMoveCmd = getCmd(getCitizenId(setup, 1), "move:left");
+        var response = Unirest.post(baseMoveCmd).body("{}").asJson();
         System.out.println(response.getBody().toPrettyString());
 
         response = Unirest.get(serverUri + "statistics").asJson();
