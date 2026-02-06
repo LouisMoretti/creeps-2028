bfce3601a97ab31514937f68933c71b42d2f8709
Louis Moretti <louis.moretti@epita.fr>
2026-02-06 15:43:27 +0100

    fireeeeeeeeee

--- src/main/java/com/epita/creeps/Program.java
+++ src/main/java/com/epita/creeps/Program.java
@@ -1,8 +1,17 @@
 package com.epita.creeps;
 
+import com.epita.creeps.given.json.Json;
+import com.epita.creeps.given.vo.geometry.Point;
+import com.epita.creeps.given.vo.parameter.FireParameter;
 import kong.unirest.core.HttpResponse;
 import kong.unirest.core.JsonNode;
+import kong.unirest.core.ObjectMapper;
 import kong.unirest.core.Unirest;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class Program {
     static String url;
@@ -44,20 +53,45 @@ public class Program {
 //        String citizen1Id = getCitizenId(setup, 1);
 //        String baseNoopCmd = serverUri + "command/" + login + "/" + citizen1Id + "/noop";
 //        String baseMoveCmd = getCmd(getCitizenId(setup, 1), "move:left");
-        String opcode = "move:left";
-        String baseBuildCmd = getCmd(getCitizenId(setup, 1), opcode);
-        var response = Unirest.post(baseBuildCmd).body("{}").asJson();
+        String opcode = "move:down";
+        String baseCmd = getCmd(getCitizenId(setup, 1), opcode);
+        var response = Unirest.post(baseCmd).body("{}").asJson();
         System.out.println(response.getBody().toPrettyString());
 
         String reportId = response.getBody().getObject().get("reportId").toString();
         waitMaison(reportId, opcode, true);
 
-        opcode = "build:road";
-        baseBuildCmd = getCmd(getCitizenId(setup, 1), opcode);
-        response = Unirest.post(baseBuildCmd).body("{}").asJson();
+//        opcode = "build:road";
+//        baseBuildCmd = getCmd(getCitizenId(setup, 1), opcode);
+//        response = Unirest.post(baseBuildCmd).body("{}").asJson();
+//        System.out.println(response.getBody().toPrettyString());
+
+        opcode = "spawn:bomber-bot";
+        baseCmd = getCmd(getCitizenId(setup, 1), opcode);
+        response = Unirest.post(baseCmd).body("{}").asJson();
         System.out.println(response.getBody().toPrettyString());
 
         reportId = response.getBody().getObject().get("reportId").toString();
+        waitMaison(reportId, opcode, true);
+
+        response = Unirest.get(serverUri + "report/" + reportId).asJson();
+        String spawnedUnitId = response.getBody().getObject().get("spawnedUnitId").toString();
+
+        int x = setup.getBody().getObject().getJSONObject("townHallCoordinates").getInt("x");
+        int y = setup.getBody().getObject().getJSONObject("townHallCoordinates").getInt("y");
+        FireParameter fireParameter = new FireParameter(new Point(x, y));
+
+        opcode = "fire:bomber-bot";
+        baseCmd = getCmd(spawnedUnitId, opcode);
+        response = Unirest.post(baseCmd).body(fireParameter).asJson();
+        System.out.println(response.getBody().toPrettyString());
+
+        reportId = response.getBody().getObject().get("reportId").toString();
+        waitMaison(reportId, opcode, true);
+
+//        response = Unirest.post(getCmd(getCitizenId(setup, 1), "noop")).body("{}").asJson();
+//        System.out.println(response.getBody().toPrettyString());
+
         waitMaison(reportId, opcode, true);
 
         response = Unirest.get(serverUri + "statistics").asJson();
