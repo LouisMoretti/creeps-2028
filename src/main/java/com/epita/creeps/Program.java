697f100fcdf44cd5464bf52e0d4119baf3c68b18
Louis Moretti <louis.moretti@epita.fr>
2026-02-07 17:41:59 +0100

    a

--- src/main/java/com/epita/creeps/Program.java
+++ src/main/java/com/epita/creeps/Program.java
@@ -356,7 +356,7 @@ public class Program {
 
 
 
-        currentPos = farmXResources(currentPos, Tile.Wood, 20, false);
+        currentPos = farmXResources(currentPos, Tile.Wood, 40, false);
 //        currentPos = goToSafePlace(currentPos, false);
 
         currentPos = goToTarget(currentPos, initResponse.townHallCoordinates.plus(1, 0), false);
